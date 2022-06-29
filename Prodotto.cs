13b0d5bdcb1ca0f94cdd5510553b0cfd3569f429
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-06-29 14:39:31 +0200

    Completo

---
+++ Prodotto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        private string codice;
+        private string nome;
+        private string descrizione;
+        private float prezzo;
+        private string iva;
+
+      
+        public Prodotto( string nome, string descrizione, float prezzo, string iva)
+        {
+            this.codice = this.RandomCode();
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+        }
+
+        public string Codice { get => codice; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Descrizione { get => descrizione; set => descrizione = value; }
+        public float Prezzo { get => prezzo; set => prezzo = value; }
+        public string Iva { get => iva; set => iva = value; }
+
+        public string RandomCode()
+        {
+            Random rnd = new Random();
+            
+            return rnd.Next(99999999).ToString(); ;
+        }
+
+        public void stampa()
+        {
+            Console.WriteLine($"Codice :{this.codice}");
+            Console.WriteLine($"Nome :{this.nome}");
+            Console.WriteLine($"Descrizione :{this.descrizione}");
+            Console.WriteLine($"Prezzo :{this.prezzo}");
+            Console.WriteLine($"IVA :{this.iva}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using csharp_oop_shop;
+
+Prodotto prodotto1 = new Prodotto("Ferrari", "Bellissima ferrari", 5000.50f, "1234567894");
+Console.WriteLine();
+prodotto1.stampa();
